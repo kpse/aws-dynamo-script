b80e3ddc61e5d864e05d7b304920bc0b80836de7
Louis Suo <louis.suo@myob.com>
2016-11-30 12:15:22 +1100

    append active column to all items

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const AWS = require('aws-sdk')
 const Promise = require('bluebird')
+const _ = require('lodash')
 
-const docClient = Promise.promisifyAll(new AWS.DynamoDB.DocumentClient({region: 'ap-southeast-2'}));
+const docClient = Promise.promisifyAll(new AWS.DynamoDB.DocumentClient({
+  region: 'ap-southeast-2'
+}));
 
-var params = {
-    TableName: "dev-louis-test-api-key3"
+const params = {
+  TableName: "dev-louis-test-api-key3"
 };
 
-docClient.scanAsync(params).then(res => console.log(JSON.stringify(res, null, 2)));
+docClient.scanAsync(params).then(res => {
+  const allItems = res.Items;
+  console.log("current: \n", JSON.stringify(allItems, null, 2))
+  _.each(allItems, i => docClient.putAsync(update(i)))
+});
+
+
+const update = (item) => {
+  return {
+    TableName: params.TableName,
+    Item: _.assign({}, item, {
+      active: true
+    })
+  }
+}
